1c1d649b778c0ed69cdd610735e1ae7e63a53709
EuphyllinKa <154064902+EuphyllinKa@users.noreply.github.com>
2025-01-13 21:54:51 +0400

    Работет ABC*2,5/6

--- app/src/main/java/com/poveev/hematomacalculator/MainActivity.java
+++ app/src/main/java/com/poveev/hematomacalculator/MainActivity.java
@@ -1,6 +1,11 @@
 package com.poveev.hematomacalculator;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.QuickContactBadge;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +15,50 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView resultTextView;
+    private EditText A_number, B_number, C_number;
+    private Button add_button;
+
+
+
+    /**
+     * @param savedInstanceState If the activity is being re-initialized after
+     *                           previously being shut down then this Bundle contains the data it most
+     *                           recently supplied in {@link #onSaveInstanceState}.  <b><i>Note: Otherwise it is null.</i></b>
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+
         });
+
+        resultTextView = findViewById(R.id.resultTextView);
+        A_number = findViewById(R.id.A_number);
+        B_number = findViewById(R.id.B_number);
+        C_number = findViewById(R.id.C_number);
+        add_button = findViewById(R.id.add_button);
+
+        add_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                float num1 = Float.parseFloat(A_number.getText().toString());
+                float num2 = Float.parseFloat(B_number.getText().toString());
+                float num3 = Float.parseFloat(C_number.getText().toString());
+                float res =(num1 * num2 * num3) * 5/2 / 6;
+                resultTextView.setText(String.valueOf(res));
+
+
+            }
+        });
+
+
     }
 }
